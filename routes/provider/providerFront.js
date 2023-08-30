acb33eb3401d9bb004082adb9ca2f4d590846be9
faizabatta1 <faizabatta1@github.com>
2023-08-31 02:07:13 +0300

    update

--- routes/provider/providerFront.js
+++ routes/provider/providerFront.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/provider', (req,res) =>{
+router.get('/providers', (req,res) =>{
     return res.status(200).render('providers/index')
 })
 
