206ef9643a7905e40a7284f21c6920dc5610e728
faizabatta1 <faizabatta1@github.com>
2023-08-31 00:50:45 +0300

    update

--- server.js
+++ server.js
@@ -18,6 +18,59 @@ app.set('view engine', 'ejs')
 const cookieParser = require('cookie-parser');
 app.use(cookieParser())
 
+app.get('/login', (req,res) =>{
+    return res.status(200).render('auth/login')
+})
+
+app.get('/api/logout',(req,res) =>{
+
+    res.cookie('isLogged',{
+        expires: Date.now()
+    })
+
+    return res.redirect('/')
+})
+
+
+app.post('/api/login', async (req,res) =>{
+    const { username, password } = req.body
+    const file = fs.readFileSync('data/credentials.json',{
+        encoding:'utf-8'
+    })
+
+    const json = JSON.parse(file)
+
+
+
+    if(username == json.username && password == json.password){
+        res.cookie('isLogged','true',{
+            maxAge: 36000000000000, // Cookie expiration time in milliseconds (1 hour in this case)
+            httpOnly: true,
+        })
+
+        res.redirect('/')
+    }else{
+        return res.status(500).json({ message: "Error Message"})
+    }
+})
+app.use((req,res,next) =>{
+    if(
+        req.url.includes('/api/')
+        || req.url.includes('/images/')
+        || req.url.includes('/profiles/')
+        || req.url.includes('/css/')
+    ){
+        return next()
+    }else{
+        if(req.cookies.isLogged == "true" && (req.url != "/login" ) ){
+            next();
+        }else{
+            return res.redirect('/login')
+        }
+    }
+
+})
+
 const userRouter = require('./routes/user/userRouter')
 app.use('/api',userRouter)
 
