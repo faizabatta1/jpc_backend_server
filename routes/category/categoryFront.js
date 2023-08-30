24d2744b6d703fccf4753a0396ed95a1895bdc9d
faizabatta1 <faizabatta1@github.com>
2023-08-31 01:44:33 +0300

    update

--- routes/category/categoryFront.js
+++ routes/category/categoryFront.js
@@ -5,21 +5,21 @@ const Category = require('../../models/Category');
 router.get('/categories', async (req, res) => {
     try {
         let categories = await Category.find({});
-        res.render('category/index', { categories });
+        res.render('categories/index', { categories });
     } catch (error) {
         res.status(500).send('Internal Server Error');
     }
 });
 
 router.get('/categories/create', (req, res) => {
-    res.render('category/create');
+    res.render('categories/create');
 });
 
 
 router.get('/categories/:id/edit', async (req, res) => {
     try {
         let category = await Category.findById(req.params.id);
-        res.render('category/edit', { category });
+        res.render('categories/edit', { category });
     } catch (error) {
         res.status(500).send('Internal Server Error');
     }
